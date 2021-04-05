6d4c339010be8183dfe0a9d7dc15ca8ae7e5ca29
32353 <3235350766@qq.com>
2021-04-06 06:14:12 +0800

    0403-0406 增添了mybatis支持，对整体框架进行了内容填充

--- src/main/java/mapper/ResultImpl.java
+++ src/main/java/mapper/ResultImpl.java
@@ -1,4 +1,44 @@
 package mapper;
 
-public class ResultImpl {
+import POJO.result;
+import org.apache.log4j.Logger;
+import service.ConnMySQL;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ResultImpl implements ResultDao {
+    Logger log = Logger.getLogger(MostRatingsImpl.class);
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
+    @Override
+    public ResultSet getResultByName(String algorithmName) {
+        String sql = "SELECT * FROM result WHERE algo = \"" + algorithmName + "\"";
+        try {
+            conn = ConnMySQL.connectMovieData("movies_data2");
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            return rs;
+        }
+        catch (SQLException SQLex){
+            log.error(SQLex);
+        }
+        catch (Exception ex) {
+            log.error(ex);
+        }
+        return null;
+    }
+
+    public void finish() {
+        try {
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            log.error(e);
+        }
+    }
 }
--- src/main/java/mapper/SearchMovieMapper.java
+++ src/main/java/mapper/SearchMovieMapper.java
@@ -1,4 +1,28 @@
 package mapper;
 
-public interface SearchMovie {
+import POJO.movie;
+import POJO.rating;
+import POJO.tag;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 类           SearchMovie
+ * 类的作用:    提供对搜索电影信息时的各种信息
+ * Description:
+ * @Date:       2021/4/5 21:07
+ * @author:     32353
+ * @version     V1.0.0
+*/
+
+public interface SearchMovieMapper {
+    /**
+     * 两个系列分别提供对电影ID、电影名的信息搜索
+     * */
+    public List<movie> searchMovieIdForInfo(int movieId);
+    public List<rating> searchMovieIdForRatings(int movieId);
+    public List<tag> searchMovieIdForTags(int movieId);
+
+    public List<movie> searchMovieNameForInfo(String movieName);
 }
